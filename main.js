b15470d0a8f22149274e2a02e36844b837ee65af
Mychenik7930 <gamerdax.play@gmail.com>
2024-12-17 23:29:38 +0900

    header

---
+++ main.js
@@ -0,0 +1,25 @@
+import Swiper from "swiper";
+import "swiper/css";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const burger = document.querySelector(".burger");
+const test_menu = document.querySelector(".test_menu");
+
+const close__button = document.querySelector(".close__button");
+const lang = document.querySelectorAll(".aside__menu--lang li a");
+const menu = document.querySelectorAll(".aside__menu--list li a");
+
+const message = document.querySelector(".message");
+const message_burger = document.querySelector(".message_burger");
+const message_tablet = document.querySelector(".message_tablet");
+const message_menu = document.querySelector(".message_menu");
+const message_menu_close = document.querySelector(".message_menu_close");
+
+const call = document.querySelector(".call");
+const call_burger = document.querySelector(".call_burger");
+const call_tablet = document.querySelector(".call_tablet");
+const call_menu = document.querySelector(".call_menu");
+const call_menu_close = document.querySelector(".call_menu_close");
